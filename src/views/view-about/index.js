99b0bb30ccd5a953306157d21c1864875a3f3eb7
Doug Rudolph <drudolph914@gmail.com>
2021-05-08 20:32:50 -0400

    adding fluff to about page

--- src/views/view-about/index.js
+++ src/views/view-about/index.js
@@ -13,6 +13,15 @@ const AboutStyle = css`
     background-color: white;
 
     padding: 10rem 10rem 0 10rem;
+
+    overflow-y: scroll;
+  }
+
+  .about-description {
+    font-family: ${fonts.wotfard};
+    font-size: 28px;
+    font-weight: 300;
+    margin-bottom: 10rem;
   }
 `
 
@@ -25,7 +34,6 @@ const MomentList = css`
   }
 
   .moment-wrapper {
-    cursor: default;
     width: 100%;
     border-radius: 2px;
     background-color: ${colors.white};
@@ -34,6 +42,7 @@ const MomentList = css`
     display: grid;
     grid-template-columns: 50% 2rem 50%;
     grid-gap: 1rem;
+
   }
 
   .moment-right {
@@ -47,7 +56,14 @@ const MomentList = css`
 
 const MomentContent = css`
   .moment-content {
+    cursor: pointer;
     padding: 2rem;
+    margin: 1rem;
+    transition: 0.2s ease-out;
+  }
+
+  .moment-content:hover {
+    box-shadow: 4px 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   }
 
   .moment-title {
@@ -64,20 +80,28 @@ const MomentContent = css`
     font-weight: 300;
   }
 
-  .about-moment-date {
-    font-family: ${fonts.wotfard};
+  .moment-date {
+    background-color: ${colors.gray20};
+    display: inline-block;
+    padding: .75rem;
+    font-family: ${fonts.roboto};
     font-size: 20px;
-    font-weight: 900;
+    font-weight: 300;
   }
 
   .moment-empty {
     display: inline-block;
-    width: 1px;;
+    width: 1px;
+    opacity: 0;
     height: auto;
   }
 `
 
 const MomentDivider = css`
+  .moment-content:hover .moment-divider-dot {
+    background-color: ${colors.orange};
+  }
+
   .moment-divider-wrapper {
     display: grid;
     justify-content: center;
@@ -106,8 +130,8 @@ const MomentDivider = css`
     border-radius: 100px;
     background-color: ${colors.black};
     display: block;
-    width: 1rem;
-    height: 1rem;
+    width: 20px;
+    height: 20px;
   }
 `
 
@@ -128,32 +152,32 @@ class About extends LitElement {
       {
         title: 'School of thought',
         description: 'Building my own education platform - schoolofthought.io',
-        date: '',
+        date: 'May 2021',
       },
       {
         title: 'Conteful Podcast',
         description: 'Conversation about coding with your voice',
-        date: '',
+        date: 'May 2021',
       },
       {
         title: 'Interseller.io',
         description: 'First engineer and building an engineering team',
-        date: '',
+        date: 'July 2019 - Current',
       },
       {
         title: 'BuzzFeed',
         description: 'Backend engineer @ BuzzFeed',
-        date: '',
+        date: 'August 2017 - Feburary 2019',
       },
       {
         title: 'BuzzFeed',
         description: 'Engineering intern @ BuzzFeed',
-        date: '',
+        date: 'May 2017 - August 2017',
       },
       {
         title: 'hackNY Fellow',
         description: 'hackNY Fellow - class of 2017',
-        date: '',
+        date: 'May 2017 - August 2017',
       },
     ]
   }
@@ -163,6 +187,7 @@ class About extends LitElement {
       return html`
         <div class="moment-wrapper moment-left">
           <div class="moment-content">
+            <div class="moment-date">${date}</div>
             <div class="moment-title">${title}</div>
             <div class="moment-description">${description}</div>
           </div>
@@ -187,6 +212,7 @@ class About extends LitElement {
           </div>
         </div>
         <div class="moment-content">
+          <div class="moment-date">${date}</div>
           <div class="moment-title">${title}</div>
           <div class="moment-description">${description}</div>
         </div>
@@ -199,9 +225,17 @@ class About extends LitElement {
       <div class="page-wrapper">
         <comp-bio></comp-bio>
         <div class="about-wrapper">
+          <div class="about-description">
+            My name is Doug. I am a Software Engineer in NYC.
+            <br><br>
+            I currently work at <a href="https://interseller.io">interseller.io</a>. I'm lucky enough to have worked at many fun places and with cool organizations along the way.
+            <br><br>
+            I like free-style soccer and chess. Follow me on twitter <a href="https://twitter.com/_dougrudolph">@_dougrudolph</a>
+          </div>
+
           <div class="moment-list">
             ${this.moments.map(({ title, description, date }, idx) => {
-              return this.renderMoment(title, description, null, idx)
+              return this.renderMoment(title, description, date, idx)
             })}
           </div>
         </div>
